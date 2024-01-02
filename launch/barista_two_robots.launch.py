964618e7de5a929c387e22afdfbe0637b0f9aa7d
Petr Busov <petr.busov@dal.ca>
2024-01-02 05:42:48 +0000

    two robots spawn and launch gazebo and rviz

---
+++ launch/barista_two_robots.launch.py
@@ -0,0 +1,81 @@
+import os
+
+from ament_index_python.packages import get_package_share_directory
+from launch import LaunchDescription
+from launch.actions import DeclareLaunchArgument
+from launch.actions import IncludeLaunchDescription
+from launch.substitutions import Command
+from launch_ros.actions import Node
+from launch.launch_description_sources import PythonLaunchDescriptionSource
+from ament_index_python.packages import get_package_prefix
+
+# this is the function launch  system will look for
+def generate_launch_description():
+
+    package_description = "barista_robot_description"
+    # RVIZ Configuration
+    rviz_config_dir = os.path.join(get_package_share_directory(package_description), 'rviz', '2barista.rviz')
+
+
+    rviz_node = Node(
+            package='rviz2',
+            executable='rviz2',
+            name='rviz_node',
+            parameters=[{'use_sim_time': True}],
+            arguments=['-d', rviz_config_dir])
+
+    pkg_gazebo_ros = get_package_share_directory('gazebo_ros')
+    pkg_box_bot_gazebo = get_package_share_directory('barista_robot_description')
+
+    # We get the whole install dir
+    # We do this to avoid having to copy or softlink manually the packages so that gazebo can find them
+    description_package_name = "barista_robot_description"
+    install_dir = get_package_prefix(description_package_name)
+
+    # Set the path to the WORLD model files. Is to find the models inside the models folder in my_box_bot_gazebo package
+    gazebo_models_path = os.path.join(pkg_box_bot_gazebo, 'models')
+    # os.environ["GAZEBO_MODEL_PATH"] = gazebo_models_path
+
+    if 'GAZEBO_MODEL_PATH' in os.environ:
+        os.environ['GAZEBO_MODEL_PATH'] =  os.environ['GAZEBO_MODEL_PATH'] + ':' + install_dir + '/share' + ':' + gazebo_models_path
+    else:
+        os.environ['GAZEBO_MODEL_PATH'] =  install_dir + "/share" + ':' + gazebo_models_path
+
+    if 'GAZEBO_PLUGIN_PATH' in os.environ:
+        os.environ['GAZEBO_PLUGIN_PATH'] = os.environ['GAZEBO_PLUGIN_PATH'] + ':' + install_dir + '/lib'
+    else:
+        os.environ['GAZEBO_PLUGIN_PATH'] = install_dir + '/lib'
+
+    
+
+    print("GAZEBO PLUGINS PATH=="+str(os.environ["GAZEBO_PLUGIN_PATH"]))
+
+    # Gazebo launch
+    gazebo = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+            os.path.join(pkg_gazebo_ros, 'launch', 'gazebo.launch.py'),
+        )
+    )
+    
+    two_robots_spawn = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource(
+        [os.path.join(pkg_box_bot_gazebo, 'launch'),'/two_robots_spawn.launch.py']))
+
+
+    # create and return launch description object
+    
+    return LaunchDescription([            
+        
+        DeclareLaunchArgument(
+          'world',
+          default_value=[os.path.join(pkg_box_bot_gazebo, 'worlds', 'box_bot_empty.world'), ''],
+          description='SDF world file'),
+        DeclareLaunchArgument(
+            'verbose', default_value='true',
+            description='Set "true" to increase messages written to terminal.'
+        ),
+        gazebo,
+        two_robots_spawn,
+        rviz_node
+        ]
+    )
